c1793c037ecf7f402175b387518d35a1d0157598
Arya <ngurahpratama2002@gmail.com>
2024-06-18 23:41:25 +0700

    adding file picker ui

--- 09 Flutter Form/Priority 2/bone_up_on/lib/contact_model.dart
+++ 09 Flutter Form/Priority 2/bone_up_on/lib/contact_model.dart
@@ -1,11 +1,15 @@
+import 'dart:ui';
+
 class Contact {
   final String name;
   final String phoneNumber;
   final DateTime date;
+  final Color color;
 
   Contact(
     this.name,
     this.phoneNumber,
-    this.date
+    this.date,
+    this.color
   );
 }
--- 09 Flutter Form/Priority 2/bone_up_on/lib/contact_page.dart
+++ 09 Flutter Form/Priority 2/bone_up_on/lib/contact_page.dart
@@ -95,6 +95,10 @@ class _ContactPageState extends State<ContactPage> {
         const SizedBox(height: 15),
         buildDatePicker(context),
         const SizedBox(height: 15),
+        buildColorPicker(context),
+        const SizedBox(height: 15),
+        buildFilePicker(context),
+        const SizedBox(height: 15),
       ],
     );
   }
@@ -136,6 +140,92 @@ class _ContactPageState extends State<ContactPage> {
     });
   }
 
+  Widget buildColorPicker(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        const Text('Color'),
+        const SizedBox(
+          height: 10,
+        ),
+        Container(
+          height: 100,
+          width: double.infinity,
+          color: _currentColor,
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Center(
+          child: ElevatedButton(
+            onPressed: () {
+              _submitColor(_currentColor);
+            },
+            child: const Text('Pick Color'),
+          ),
+        )
+      ],
+    );
+  }
+
+  void _submitColor(Color color) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Pick Your Color'),
+          content: BlockPicker(
+            pickerColor: _currentColor,
+            onColorChanged: (color) {
+              setState(() {
+                _currentColor = color;
+              });
+            },
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Save'),
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  Widget buildFilePicker(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Pick Files'),
+        const SizedBox(height: 10),
+        Center(
+          child: ElevatedButton(
+            onPressed: () {
+              _pickFile();
+            },
+            child: const Text('Pick and open file'),
+          ),
+        )
+      ],
+    );
+  }
+
+  void _pickFile() async {
+    final result = await FilePicker.platform.pickFiles();
+    if (result == null) return;
+
+    // Mendapatkan file dari result
+    final file = result.files.first;
+    // _openFile(file);
+  }
+
+  void _openFile(PlatformFile file) {
+    OpenFile.open(file.path);
+  }
+
   Widget submitButton() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
@@ -159,11 +249,20 @@ class _ContactPageState extends State<ContactPage> {
       final name = nameController.text;
       final number = numberController.text;
       final date = _dueDate;
+      final color = _currentColor;
       submittedData.add(Contact(
         name,
         number,
         date,
+        color,
       ));
+      for (var contact in submittedData) {
+        print('Name: ${contact.name}');
+        print('Phone Number: ${contact.phoneNumber}');
+        print('Date: ${contact.date}');
+        print('Color: ${contact.color}');
+        print('-------------------');
+      }
       isDataSubmitted = true;
       nameController.clear();
       numberController.clear();
@@ -194,13 +293,41 @@ class _ContactPageState extends State<ContactPage> {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return DeleteContactDialog(
-          onConfirm: () {
-            setState(() {
-              submittedData.removeAt(index);
-            });
-            Navigator.of(context).pop();
-          },
+        return AlertDialog(
+          title: const Text('Confirm Deletion'),
+          content: const Text('Are you sure you want to delete this contact?'),
+          actions: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  submittedData.removeAt(index);
+                });
+                Navigator.of(context).pop();
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                backgroundColor: const Color(0xFF6750A4),
+              ),
+              child: const Text('Yes'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                backgroundColor: Colors.white,
+              ),
+              child: const Text(
+                'No',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ],
         );
       },
     );
@@ -212,6 +339,7 @@ class _ContactPageState extends State<ContactPage> {
     TextEditingController phoneNumberController =
         TextEditingController(text: contact.phoneNumber);
     DateTime _editingDate = contact.date;
+    Color _editingcolor = contact.color;
 
     showDialog(
       context: context,
@@ -249,23 +377,62 @@ class _ContactPageState extends State<ContactPage> {
                       });
                     },
                     child: const Text('Select'),
-                  )
+                  ),
                 ],
               ),
               Text(
                 DateFormat('dd-MM-yyyy').format(_editingDate),
               ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 100,
+                width: double.infinity,
+                color: _editingcolor,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: const Text('Pick Your Color'),
+                          content: BlockPicker(
+                            pickerColor: _editingcolor,
+                            onColorChanged: (color) {
+                              setState(() {
+                                _editingcolor = color;
+                              });
+                            },
+                          ),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: const Text('Save'),
+                            )
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: const Text('Pick Color'),
+                ),
+              )
             ],
           ),
           actions: [
             ElevatedButton(
               onPressed: () {
                 setState(() {
-                  submittedData[index] = Contact(
-                    nameController.text,
-                    phoneNumberController.text,
-                    _editingDate,
-                  );
+                  submittedData[index] = Contact(nameController.text,
+                      phoneNumberController.text, _editingDate, _editingcolor);
                 });
                 Navigator.of(context).pop();
               },
@@ -317,6 +484,18 @@ class ContactListItem extends StatelessWidget {
         children: [
           Text(contact.phoneNumber),
           Text(_formatDate(contact.date)),
+          Row(
+            children: [
+              Container(
+                width: 20,
+                height: 20,
+                color: contact.color,
+              ),
+              const SizedBox(width: 8),
+              Text(
+                  'Color: ${contact.color.value.toRadixString(16).padLeft(9, '#').toUpperCase()}'),
+            ],
+          )
         ],
       ),
       trailing: Row(
@@ -361,46 +540,5 @@ class ContactListItem extends StatelessWidget {
       default:
         return dayOfWeek;
     }
-  }
-}
-
-class DeleteContactDialog extends StatelessWidget {
-  final VoidCallback onConfirm;
-
-  const DeleteContactDialog({super.key, required this.onConfirm});
-
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Text('Confirm Deletion'),
-      content: const Text('Are you sure you want to delete this contact?'),
-      actions: <Widget>[
-        ElevatedButton(
-          onPressed: onConfirm,
-          style: ElevatedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20.0),
-            ),
-            backgroundColor: const Color(0xFF6750A4),
-          ),
-          child: const Text('Yes'),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          style: ElevatedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20.0),
-            ),
-            backgroundColor: Colors.white,
-          ),
-          child: const Text(
-            'No',
-            style: TextStyle(color: Colors.black),
-          ),
-        ),
-      ],
-    );
   }
 }
